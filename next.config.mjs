f27f288d77e302c19d1819134542c02bc5f0cf69
doinel1a <doinel1atanasiu@gmail.com>
2024-03-11 16:20:33 +0100

    feat: install & configure rainbowkit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true
+  },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
   }
 };
 
