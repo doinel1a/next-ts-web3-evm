392f6f53622c3735d6b81df573602a1cf442c5b3
doinel1a <doinel1atanasiu@gmail.com>
2024-03-12 20:42:39 +0100

    feat(wallet-dropdown): implement switch network into actions section

--- src/components/wallet/dropdown/index.tsx
+++ src/components/wallet/dropdown/index.tsx
@@ -14,6 +14,7 @@ import {
 
 import CopyAddress from './actions/copy-address';
 import QRCodeDialog from './actions/dialog/qrcode';
+import SwitchNetworkDialog from './actions/dialog/switch-network';
 import Disconnect from './actions/disconnect';
 import Balance from './details/balance';
 import ENSName from './details/ens-name';
@@ -22,6 +23,7 @@ import Network from './details/network';
 export default function WalletDropdown() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isQRCodeDialogOpen, setIsQRCodeDialogOpen] = useState(false);
+  const [isSwitchNetworkDialogOpen, setIsSwitchNetworkDialogOpen] = useState(false);
 
   const dropdownTriggerReference = useRef<HTMLButtonElement | null>(null);
   const focusReference = useRef<HTMLButtonElement | null>(null);
@@ -49,7 +51,7 @@ export default function WalletDropdown() {
       </DropdownMenuTrigger>
       <DropdownMenuContent
         className='w-56'
-        hidden={isQRCodeDialogOpen}
+        hidden={isQRCodeDialogOpen || isSwitchNetworkDialogOpen}
         onCloseAutoFocus={(event) => {
           if (focusReference.current) {
             focusReference.current.focus();
@@ -76,6 +78,12 @@ export default function WalletDropdown() {
             address={address}
             onDropdownSelect={handleDropdownItemSelect}
             onDialogOpenChange={(open) => setIsQRCodeDialogOpen(open)}
+          />
+          <SwitchNetworkDialog
+            isDialogOpen={isSwitchNetworkDialogOpen}
+            setIsDropdownOpen={setIsDropdownOpen}
+            onDropdownSelect={handleDropdownItemSelect}
+            onDialogOpenChange={(open) => setIsSwitchNetworkDialogOpen(open)}
           />
           <Disconnect />
         </DropdownMenuGroup>
