0056e2d9e3fe47da9c19cec2a32a4d2a45a0b06b
doinel1a <doinel1atanasiu@gmail.com>
2024-03-12 19:44:42 +0100

    feat(wallet-dropdown): implement copy address from qrcode into actions section

--- src/components/wallet/dropdown/index.tsx
+++ src/components/wallet/dropdown/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 
 import { Button } from '@nextui-org/button';
 import { useAccount } from 'wagmi';
@@ -13,12 +13,17 @@ import {
 } from '@/components/ui/dropdown-menu';
 
 import CopyAddress from './actions/copy-address';
+import QRCodeDialog from './actions/dialog/qrcode';
 import Balance from './details/balance';
 import ENSName from './details/ens-name';
 import Network from './details/network';
 
 export default function WalletDropdown() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isQRCodeDialogOpen, setIsQRCodeDialogOpen] = useState(false);
+
+  const dropdownTriggerReference = useRef<HTMLButtonElement | null>(null);
+  const focusReference = useRef<HTMLButtonElement | null>(null);
 
   const { address } = useAccount();
   const displayAddress = useMemo(
@@ -26,17 +31,33 @@ export default function WalletDropdown() {
     [address]
   );
 
+  function handleDropdownItemSelect() {
+    focusReference.current = dropdownTriggerReference.current;
+  }
+
   return (
     <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
       <DropdownMenuTrigger asChild>
         <Button
+          ref={dropdownTriggerReference}
           color='default'
           onClick={() => setIsDropdownOpen((previousState) => !previousState)}
         >
           {displayAddress}
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className='w-56'>
+      <DropdownMenuContent
+        className='w-56'
+        hidden={isQRCodeDialogOpen}
+        onCloseAutoFocus={(event) => {
+          if (focusReference.current) {
+            focusReference.current.focus();
+            focusReference.current = null;
+
+            event.preventDefault();
+          }
+        }}
+      >
         <DropdownMenuLabel>Details</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
@@ -49,6 +70,12 @@ export default function WalletDropdown() {
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <CopyAddress address={address} />
+          <QRCodeDialog
+            isDialogOpen={isQRCodeDialogOpen}
+            address={address}
+            onDropdownSelect={handleDropdownItemSelect}
+            onDialogOpenChange={(open) => setIsQRCodeDialogOpen(open)}
+          />
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
