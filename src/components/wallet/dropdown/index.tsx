dec7ee343938d6303f9aec7c65622ad07f0c2e4d
doinel1a <doinel1atanasiu@gmail.com>
2024-03-12 19:45:49 +0100

    feat(wallet-dropdown): implement disconnect into actions section

--- src/components/wallet/dropdown/index.tsx
+++ src/components/wallet/dropdown/index.tsx
@@ -14,6 +14,7 @@ import {
 
 import CopyAddress from './actions/copy-address';
 import QRCodeDialog from './actions/dialog/qrcode';
+import Disconnect from './actions/disconnect';
 import Balance from './details/balance';
 import ENSName from './details/ens-name';
 import Network from './details/network';
@@ -76,6 +77,7 @@ export default function WalletDropdown() {
             onDropdownSelect={handleDropdownItemSelect}
             onDialogOpenChange={(open) => setIsQRCodeDialogOpen(open)}
           />
+          <Disconnect />
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
