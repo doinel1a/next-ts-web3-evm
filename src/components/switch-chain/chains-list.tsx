508199092c53dba97cff78e2ee85ff5989659cd3
doinel1a <doinel1atanasiu@gmail.com>
2024-09-20 08:11:47 +0200

    refactor(wallet/dialog/actions/switch-chain): extract dialog content & switch chain login to reuse

---
+++ src/components/switch-chain/chains-list.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import React from 'react';
+
+import type { Chain } from 'viem';
+
+import { Button } from '@nextui-org/button';
+
+type TChainsList = {
+  title: string;
+  activeChainId: number;
+  pendingChainId: number;
+  isSwitchSuccess: boolean;
+  isSwitchError: boolean;
+  chainsList: Chain[];
+  onSwitchChain: (chainId: number) => Promise<void>;
+};
+
+export default function ChainsList({
+  title,
+  activeChainId,
+  pendingChainId,
+  isSwitchSuccess,
+  isSwitchError,
+  chainsList,
+  onSwitchChain
+}: TChainsList) {
+  return (
+    <div className='flex flex-col gap-y-1.5'>
+      <h3>{title}</h3>
+      <ul className='flex flex-col gap-y-2.5 pl-1.5'>
+        {chainsList.map((chain) => (
+          <li key={chain.id} className='relative flex h-10 w-full items-center justify-end'>
+            <Button
+              color='secondary'
+              className='w-full items-center justify-start'
+              onClick={() => onSwitchChain(chain.id)}
+            >
+              {chain.name}
+            </Button>
+
+            {!isSwitchSuccess && chain.id === pendingChainId ? (
+              isSwitchError ? (
+                <div className='pointer-events-none absolute right-4 flex items-center gap-x-2.5'>
+                  <span className='text-sm'>Failed</span>
+                  <span className='h-2 w-2 rounded-full bg-red-400' />
+                </div>
+              ) : (
+                <div className='pointer-events-none absolute right-4 flex items-center gap-x-2.5'>
+                  <span className='text-sm'>Approve in Wallet</span>
+                  <span className='h-2 w-2 animate-pulse rounded-full bg-yellow-400' />
+                </div>
+              )
+            ) : null}
+
+            {chain.id === activeChainId ? (
+              <div className='pointer-events-none absolute right-4 flex items-center gap-x-2.5'>
+                <span className='text-sm'>Connected</span>
+                <span className='h-2 w-2 rounded-full bg-green-400' />
+              </div>
+            ) : null}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/components/wallet/dropdown/actions/dialog/switch-chain.tsx
+++ src/components/wallet/dropdown/actions/dialog/switch-chain.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback } from 'react';
 
-import type { Chain } from 'viem';
-
-import { Button } from '@nextui-org/button';
 import { RefreshCcw } from 'lucide-react';
-import { toast } from 'sonner';
-import { useChainId, useSwitchChain } from 'wagmi';
 
-import { DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import SwitchChainDialogContent from '@/components/switch-chain/dialog-content';
+import useSwitchChain from '@/hooks/use-switch-chain';
 
 import ForwardedDialog from '.';
 import IconDropdownMenuItem from '../../commons/icon-item';
@@ -27,76 +23,21 @@ export default function SwitchChainDialog({
   onDropdownSelect,
   onDialogOpenChange
 }: TSwitchChainDialog) {
-  const activeChainId = useChainId();
-  const { chains, variables, isError, isSuccess, reset, switchChainAsync } = useSwitchChain();
-  const mainnetChains = useMemo(() => chains.filter((chain) => !!!chain.testnet), [chains]);
-  const testnetChains = useMemo(() => chains.filter((chain) => !!chain.testnet), [chains]);
+  const onSwitchSuccessCallback = useCallback(() => {
+    onDialogOpenChange(false);
+    setIsDropdownOpen(false);
+  }, [onDialogOpenChange, setIsDropdownOpen]);
 
-  const activeChain = useMemo(
-    () => chains.find((chain) => chain.id === activeChainId),
-    [activeChainId, chains]
-  );
-
-  const onSwitchChain = useCallback(
-    async (chainId: number) => {
-      if (activeChainId === chainId) {
-        return;
-      }
-
-      try {
-        await switchChainAsync({ chainId });
-
-        onDialogOpenChange(false);
-        setIsDropdownOpen(false);
-
-        toast.success('Success', {
-          description: (
-            <>
-              {activeChain ? (
-                <p>
-                  Chain switched to <span className='font-semibold'>{activeChain.name}</span>.
-                </p>
-              ) : (
-                <p>Chain switched.</p>
-              )}
-            </>
-          )
-        });
-      } catch (error: unknown) {
-        if (
-          error !== null &&
-          error !== undefined &&
-          typeof error === 'object' &&
-          'name' in error &&
-          typeof error.name === 'string'
-        ) {
-          let errorMessage = '';
-
-          switch (error.name) {
-            case 'UserRejectedRequestError': {
-              errorMessage = 'The switch chain request was rejected.';
-              break;
-            }
-            case 'SwitchChainError': {
-              errorMessage = 'A switch chain request is already pending.';
-              break;
-            }
-            default: {
-              errorMessage = 'Something horribly wrong happened.';
-              break;
-            }
-          }
-
-          toast.error('Error', {
-            description: errorMessage
-          });
-        }
-
-        console.error('Error switching chain', error);
-      }
-    },
-    [activeChainId, activeChain, onDialogOpenChange, setIsDropdownOpen, switchChainAsync]
-  );
+  const {
+    activeChainId,
+    mainnetChains,
+    testnetChains,
+    variables,
+    isError,
+    isSuccess,
+    reset,
+    onSwitchChain
+  } = useSwitchChain(onSwitchSuccessCallback);
 
   return (
     <ForwardedDialog
@@ -108,91 +49,15 @@ export default function SwitchChainDialog({
         reset();
       }}
     >
-      <DialogHeader>
-        <DialogTitle>Switch Chain</DialogTitle>
-      </DialogHeader>
-
-      <div className='flex flex-col gap-y-3'>
-        <ChainsList
-          title='Mainnet'
-          activeChainId={activeChainId}
-          pendingChainId={variables?.chainId ?? 0}
-          isSwitchSuccess={isSuccess}
-          isSwitchError={isError}
-          chainsList={mainnetChains}
-          onSwitchChain={onSwitchChain}
-        />
-
-        <ChainsList
-          title='Testnet'
-          activeChainId={activeChainId}
-          pendingChainId={variables?.chainId ?? 0}
-          isSwitchSuccess={isSuccess}
-          isSwitchError={isError}
-          chainsList={testnetChains}
-          onSwitchChain={onSwitchChain}
-        />
-      </div>
+      <SwitchChainDialogContent
+        activeChainId={activeChainId}
+        pendingChainId={variables?.chainId ?? 0}
+        isSwitchSuccess={isSuccess}
+        isSwitchError={isError}
+        mainnetChains={mainnetChains}
+        testnetChains={testnetChains}
+        onSwitchChain={onSwitchChain}
+      />
     </ForwardedDialog>
-  );
-}
-
-type TChainsList = {
-  title: string;
-  activeChainId: number;
-  pendingChainId: number;
-  isSwitchSuccess: boolean;
-  isSwitchError: boolean;
-  chainsList: Chain[];
-  onSwitchChain: (chainId: number) => Promise<void>;
-};
-
-function ChainsList({
-  title,
-  activeChainId,
-  pendingChainId,
-  isSwitchSuccess,
-  isSwitchError,
-  chainsList,
-  onSwitchChain
-}: TChainsList) {
-  return (
-    <div className='flex flex-col gap-y-1.5'>
-      <h3>{title}</h3>
-      <ul className='flex flex-col gap-y-2.5 pl-1.5'>
-        {chainsList.map((chain) => (
-          <li key={chain.id} className='relative flex h-10 w-full items-center justify-end'>
-            <Button
-              color='secondary'
-              className='w-full items-center justify-start'
-              onClick={() => onSwitchChain(chain.id)}
-            >
-              {chain.name}
-            </Button>
-
-            {!isSwitchSuccess && chain.id === pendingChainId ? (
-              isSwitchError ? (
-                <div className='pointer-events-none absolute right-4 flex items-center gap-x-2.5'>
-                  <span className='text-sm'>Failed</span>
-                  <span className='h-2 w-2 rounded-full bg-red-400' />
-                </div>
-              ) : (
-                <div className='pointer-events-none absolute right-4 flex items-center gap-x-2.5'>
-                  <span className='text-sm'>Approve in Wallet</span>
-                  <span className='h-2 w-2 animate-pulse rounded-full bg-yellow-400' />
-                </div>
-              )
-            ) : null}
-
-            {chain.id === activeChainId ? (
-              <div className='pointer-events-none absolute right-4 flex items-center gap-x-2.5'>
-                <span className='text-sm'>Connected</span>
-                <span className='h-2 w-2 rounded-full bg-green-400' />
-              </div>
-            ) : null}
-          </li>
-        ))}
-      </ul>
-    </div>
   );
 }
